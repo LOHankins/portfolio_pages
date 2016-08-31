463968c2a69440db63623d6dcff907a5ccd31bf4
Louis <LOHankins@gmail.com>
2016-08-31 17:48:07 -0400

    added detail parts of resume

---
+++ jscript/portfolio.js
@@ -0,0 +1,180 @@
+$(document).on("ready", function() {
+	event.preventDefault();
+
+var index;
+
+
+function handleClickSummaryButton1() {
+	console.log("handleClickSummaryButton1");
+	$(".summary1").css("display", "inline-block");
+	$(".detail1").css("display", "none");
+
+}
+function handleClickDetailButton1() {
+	console.log("handleClickDetailButton1");
+	$(".summary1").css("display", "none");
+	$(".detail1").css("display", "inline-block");
+
+}
+function handleClickSummaryButton2() {
+	console.log("handleClickSummaryButton2");
+	$(".summary2").css("display", "inline-block");
+	$(".detail2").css("display", "none");
+
+}
+function handleClickDetailButton2() {
+	console.log("handleClickDetailButton2");
+	$(".summary2").css("display", "none");
+	$(".detail2").css("display", "inline-block");
+
+}
+function handleClickSummaryButton3() {
+	console.log("handleClickSummaryButton3");
+	$(".summary3").css("display", "inline-block");
+	$(".detail3").css("display", "none");
+
+}
+function handleClickDetailButton3() {
+	console.log("handleClickDetailButton3");
+	$(".summary3").css("display", "none");
+	$(".detail3").css("display", "inline-block");
+
+}
+function handleClickSummaryButton4() {
+	console.log("handleClickSummaryButton4");
+	$(".summary4").css("display", "inline-block");
+	$(".detail4").css("display", "none");
+
+}
+function handleClickDetailButton4() {
+	console.log("handleClickDetailButton4");
+	$(".summary4").css("display", "none");
+	$(".detail4").css("display", "inline-block");
+
+}
+function handleClickSummaryButton5() {
+	console.log("handleClickSummaryButton5");
+	$(".summary5").css("display", "inline-block");
+	$(".detail5").css("display", "none");
+
+}
+function handleClickDetailButton5() {
+	console.log("handleClickDetailButton5");
+	$(".summary5").css("display", "none");
+	$(".detail5").css("display", "inline-block");
+
+}
+function handleClickSummaryButton6() {
+	console.log("handleClickSummaryButton6");
+	$(".summary6").css("display", "inline-block");
+	$(".detail6").css("display", "none");
+
+}
+function handleClickDetailButton6() {
+	console.log("handleClickDetailButton6");
+	$(".summary6").css("display", "none");
+	$(".detail6").css("display", "inline-block");
+
+}
+function handleClickSummaryButton7() {
+	console.log("handleClickSummaryButton7");
+	$(".summary7").css("display", "inline-block");
+	$(".detail7").css("display", "none");
+
+}
+function handleClickDetailButton7() {
+	console.log("handleClickDetailButton7");
+	$(".summary7").css("display", "none");
+	$(".detail7").css("display", "inline-block");
+
+}
+function handleClickSummaryButton8() {
+	console.log("handleClickSummaryButton8");
+	$(".summary8").css("display", "inline-block");
+	$(".detail8").css("display", "none");
+
+}
+function handleClickDetailButton8() {
+	console.log("handleClickDetailButton8");
+	$(".summary8").css("display", "none");
+	$(".detail8").css("display", "inline-block");
+
+}
+function handleClickSummaryButton9() {
+	console.log("handleClickSummaryButton9");
+	$(".summary9").css("display", "inline-block");
+	$(".detail9").css("display", "none");
+
+}
+function handleClickDetailButton9() {
+	console.log("handleClickDetailButton9");
+	$(".summary9").css("display", "none");
+	$(".detail9").css("display", "inline-block");
+
+}
+function handleClickSummaryButton10() {
+	console.log("handleClickSummaryButton10");
+	$(".summary10").css("display", "inline-block");
+	$(".detail10").css("display", "none");
+
+}
+function handleClickDetailButton10() {
+	console.log("handleClickDetailButton10");
+	$(".summary10").css("display", "none");
+	$(".detail10").css("display", "inline-block");
+
+}
+function handleClickSummaryButton11() {
+	console.log("handleClickSummaryButton11");
+	$(".summary11").css("display", "inline-block");
+	$(".detail11").css("display", "none");
+
+}
+function handleClickDetailButton11() {
+	console.log("handleClickDetailButton11");
+	$(".summary11").css("display", "none");
+	$(".detail11").css("display", "inline-block");
+
+}
+function handleClickSummaryButton12() {
+	console.log("handleClickSummaryButton12");
+	$(".summary12").css("display", "inline-block");
+	$(".detail12").css("display", "none");
+
+}
+function handleClickDetailButton12() {
+	console.log("handleClickDetailButton12");
+	$(".summary12").css("display", "none");
+	$(".detail12").css("display", "inline-block");
+
+}
+
+console.log("portfolio.js");
+$("#summary1").click(handleClickSummaryButton1);
+$("#detail1").click(handleClickDetailButton1);
+$("#summary2").click(handleClickSummaryButton2);
+$("#detail2").click(handleClickDetailButton2);
+$("#summary3").click(handleClickSummaryButton3);
+$("#detail3").click(handleClickDetailButton3);
+$("#summary4").click(handleClickSummaryButton4);
+$("#detail4").click(handleClickDetailButton4);
+$("#summary5").click(handleClickSummaryButton5);
+$("#detail5").click(handleClickDetailButton5);
+$("#summary6").click(handleClickSummaryButton6);
+$("#detail6").click(handleClickDetailButton6);
+$("#summary7").click(handleClickSummaryButton7);
+$("#detail7").click(handleClickDetailButton7);
+$("#summary8").click(handleClickSummaryButton8);
+$("#detail8").click(handleClickDetailButton8);
+$("#summary9").click(handleClickSummaryButton9);
+$("#detail9").click(handleClickDetailButton9);
+$("#summary10").click(handleClickSummaryButton10);
+$("#detail10").click(handleClickDetailButton10);
+$("#summary11").click(handleClickSummaryButton11);
+$("#detail11").click(handleClickDetailButton11);
+$("#summary12").click(handleClickSummaryButton12);
+$("#detail12").click(handleClickDetailButton12);
+
+
+
+});
